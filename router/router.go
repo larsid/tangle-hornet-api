207f6d9a22e4058e953942f69331eb9d4a73feac
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-10 16:13:22 -0300

    feat: create router

---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/allancapistrano/tangle-hornet-api/endpoints"
+	"github.com/gorilla/mux"
+)
+
+func HandleRequests() (router *mux.Router) {
+	router = mux.NewRouter().StrictSlash(true)
+
+	// Root
+	router.HandleFunc("/", endpoints.Root)
+
+	// Messages
+	router.HandleFunc("/message", endpoints.CreateNewMessage).Methods("POST")
+	router.HandleFunc("/message/{index}", endpoints.GetAllMessagesByIndex)
+
+	return router
+}
