79643b244762fad2a94d561d872585b911523893
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-10 16:04:06 -0300

    feat: create CreateNewMessage function

--- endpoints/message.go
+++ endpoints/message.go
@@ -1,15 +1,31 @@
 package endpoints
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Message struct {
+	Index   string `json:"index"`
+	Content string `json:"content"`
+}
+
 func GetAllMessagesByIndex(writer http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-	index:= vars["index"]
+	index := vars["index"]
 
-	fmt.Fprintf(writer, "Retornando todas as mensagens do índice: " + index)
+	fmt.Fprintf(writer, "Retornando todas as mensagens do índice: "+index)
+}
+
+func CreateNewMessage(writer http.ResponseWriter, request *http.Request) {
+	var message Message
+	
+	requestBody, _ := io.ReadAll(request.Body)
+
+	json.Unmarshal(requestBody, &message)
+	json.NewEncoder(writer).Encode(message)
 }
