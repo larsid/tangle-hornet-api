9714495ab8cd29889d7b0d47cd842ebcf7edad41
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-10 16:13:32 -0300

    chore: using router

--- main.go
+++ main.go
@@ -5,28 +5,15 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-
-	"github.com/allancapistrano/tangle-hornet-api/endpoints"
+	"github.com/allancapistrano/tangle-hornet-api/router"
 )
 
 const PORT = 3000
 
-func handleRequests() {
-	router := mux.NewRouter().StrictSlash(true)
-
-	// Root
-	router.HandleFunc("/", endpoints.Root)
-
-	// Messages
-	router.HandleFunc("/message", endpoints.CreateNewMessage).Methods("POST")
-	router.HandleFunc("/message/{index}", endpoints.GetAllMessagesByIndex)
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), router))
-}
-
 func main() {
 	fmt.Printf("Starting server on port %d", PORT)
 
-	handleRequests()
+	router := router.HandleRequests()
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), router))
 }
