17928dc44986d10d69c425441b3a75a95334e4d4
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-19 22:03:22 -0300

    refactor: using the config file

--- endpoints/message.go
+++ endpoints/message.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/allancapistrano/tangle-client-go/messages"
+	"github.com/allancapistrano/tangle-hornet-api/config"
 	"github.com/gorilla/mux"
 )
 
@@ -19,18 +20,20 @@ type Message struct {
 // Get all messages using a specific index.
 func GetAllMessagesByIndex(writer http.ResponseWriter, request *http.Request) {
 	var jsonInString string
-	nodeURL := "http://127.0.0.1:14265"
+	nodeURL := config.GetNodeUrl(CONFIG_FILE_NAME, true)
+	nodePort := config.GetNodePort(CONFIG_FILE_NAME, true)
+	nodeAddress := fmt.Sprintf("http://%s:%s", nodeURL, nodePort)
 
 	vars := mux.Vars(request)
 	index := vars["index"]
 
-	messagesByIndex, err := messages.GetAllMessagesByIndex(nodeURL, index)
+	messagesByIndex, err := messages.GetAllMessagesByIndex(nodeAddress, index)
 
 	if err != nil {
 		jsonInString = fmt.Sprintf("{\"error\": \"%s\"}", err.Error())
 	} else {
 		jsonInBytes, err := json.Marshal(messagesByIndex)
-		
+
 		if err != nil {
 			jsonInString = "{\"error\": \"Unable to convert the messages struct into JSON format.\"}"
 		} else {
@@ -44,13 +47,15 @@ func GetAllMessagesByIndex(writer http.ResponseWriter, request *http.Request) {
 // Create and submit a new message.
 func CreateNewMessage(writer http.ResponseWriter, request *http.Request) {
 	var message Message
-	nodeURL := "http://127.0.0.1:14265"
+	nodeURL := config.GetNodeUrl(CONFIG_FILE_NAME, true)
+	nodePort := "14265"
+	nodeAddress := fmt.Sprintf("http://%s:%s", nodeURL, nodePort)
 
 	requestBody, _ := io.ReadAll(request.Body)
 
 	json.Unmarshal(requestBody, &message)
 
-	isMessageCreated := messages.SubmitMessage(nodeURL, message.Index, message.Content, 15)
+	isMessageCreated := messages.SubmitMessage(nodeAddress, message.Index, message.Content, 15)
 
 	if isMessageCreated {
 		json.NewEncoder(writer).Encode(message)
--- endpoints/root.go
+++ endpoints/root.go
@@ -6,15 +6,20 @@ import (
 	"net/http"
 
 	"github.com/allancapistrano/tangle-client-go/info"
+	"github.com/allancapistrano/tangle-hornet-api/config"
 )
+
+const CONFIG_FILE_NAME = "tangle-hornet.conf"
 
 // Shows information about Tangle Hornet Network
 func Root(writer http.ResponseWriter, request *http.Request) {
 	var jsonInString string
-	nodeURL := "http://127.0.0.1:14265"
+	nodeURL := config.GetNodeUrl(CONFIG_FILE_NAME, true)
+	nodePort := config.GetNodePort(CONFIG_FILE_NAME, true)
+	nodeAddress := fmt.Sprintf("http://%s:%s", nodeURL, nodePort)
 
 	// Network info
-	nodeInfo, err := info.GetNodeInfo(nodeURL)
+	nodeInfo, err := info.GetNodeInfo(nodeAddress)
 
 	if err != nil {
 		jsonInString = fmt.Sprintf("{\"error\": \"%s\"}", err.Error())
