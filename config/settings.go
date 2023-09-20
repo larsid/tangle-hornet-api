496d3654bf2940aebe550ae4e1171fac31fa2dc9
AllanCapistrano <allan.capistrano3@gmail.com>
2023-09-19 21:55:36 -0300

    feat: create GetNodeUrl function

--- config/settings.go
+++ config/settings.go
@@ -47,3 +47,43 @@ func GetApiPort(fileName string, debug bool) string {
 
 	return apiPort
 }
+
+// Get the node URL used by the Tangle Hornet node.
+func GetNodeUrl(fileName string, debug bool) string {
+	nodeUrl := "127.0.0.1"
+	foundSettingsFile := true
+	directory := "/etc/"
+
+	filePath := filepath.Join(directory, fileName)
+	file, err := os.Open(filePath)
+	if err != nil {
+		if debug {
+			fmt.Printf(
+				"Couldn't open the '%s' file! Check the path or file name.\n",
+				fileName,
+			)
+		}
+		foundSettingsFile = false
+	}
+
+	defer file.Close()
+
+	if foundSettingsFile {
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+
+		for scanner.Scan() {
+			// Ignore the comments.
+			if strings.Contains(scanner.Text(), "#") {
+				continue
+			}
+
+			// Found API port setting.
+			if strings.Contains(scanner.Text(), "nodeUrl") {
+				nodeUrl = strings.Split(scanner.Text(), " = ")[1]
+			}
+		}
+	}
+
+	return nodeUrl
+}
